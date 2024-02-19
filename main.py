32b88ada759920ab89bd58aeb44747545d4e1f28
Deepanshu0810 <deepanshu0810@gmail.com>
2024-02-20 01:01:15 +0530

    feat: path parameters and query parameters

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
-@app.get("/")   
-def get_items():
-    return {"items": [{"name": "Item One"}, {"name": "Item Two"}]}
+@app.get("/blog")   #path operation decorator; operation; path
+# path operation function
+def index(limit:int = 10, published:bool = True, sort: Optional[str] = None): #query parameter
+    if published:
+        return {"data": f"blog list {limit} published blogs"}
+    else:
+        return {"data": f"blog list {limit}"}
 
-@app.get("/about")
-def about():
-    return {"about": "This is a simple API for learning purposes."}
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished blogs"}
+
+# dynamic paths come after static paths
+@app.get("/blog/{id}")
+def show(id: int): #pydantic model
+    return {"data":id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id:int, limit:int = 10): #query parameter to get 10 comments
+    return {"data": {"1", "2"}}
